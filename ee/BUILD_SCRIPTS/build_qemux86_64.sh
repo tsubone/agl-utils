a13a1a4f120094689698170ab4a99d3c38243740
hoge <you@example.com>
2018-01-25 15:09:29 +0000

    update.

--- ee/BUILD_SCRIPTS/build_qemux86_64.sh
+++ ee/BUILD_SCRIPTS/build_qemux86_64.sh
@@ -5,7 +5,10 @@ BRANCH=eel
 AGL_VER=eel_5.0.0.xml
 AGL_SETUP_TARGET='agl-demo agl-devel'
 
+echo "repo init -b ${BRANCH} -m ${AGL_VER} -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo"
 repo init -b ${BRANCH} -m ${AGL_VER} -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
+
+echo "repo sync"
 repo sync
 
 export MACHINE=${MACHINE}
